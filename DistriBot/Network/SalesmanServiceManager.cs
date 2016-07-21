8f5cd1ff46a53e1d04249d038e4ad32a4cc1c94d
Alejandro Monetti <alejandromonetti13@gmail.com>
2016-07-20 23:30:19 -0300

    SalesmanServiceManager

---
+++ DistriBot/Network/SalesmanServiceManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Json;
+
+
+namespace DistriBot
+{
+	public class SalesmanServiceManager
+	{
+		
+		public SalesmanServiceManager()
+		{
+		}
+
+		public static void Login(string username, string password, Action success, Action failure)
+		{
+			string grant_type = "password";
+			string relativeUrl = "";
+			JsonObject parameters = new JsonObject();
+			parameters.Add("username", username);
+			parameters.Add("password", password);
+			parameters.Add("grant_type", grant_type);
+			HTTPHelper.GetInstance().PostRequest(relativeUrl, parameters, success: (obj) =>
+			{
+				//TODO: Success
+			}, failure: (obj) =>
+			{
+				//TODO: Failure
+			});
+
+		}
+	}
+}
+
