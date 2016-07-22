b74b769df634f2846f81891438cc6d2a32e31c85
federicozaiter <fzaiter@gmail.com>
2016-07-22 18:59:16 -0300

    Se generó la interfaz sin la funcionalidad de login; se agregaron colores, estilo e imagen a los recursos.

--- DistriBot/MainActivity.cs
+++ DistriBot/MainActivity.cs
@@ -1,32 +1,46 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
 
 namespace DistriBot
 {
-	[Activity(Label = "DistriBot", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	[Activity(Label = "DistriBot", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar.FullScreen")]
+	public class MainActivity : AppCompatActivity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			var btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            var btnForgotPassword = FindViewById<TextView>(Resource.Id.tvForgotPasswordLink);
+            var etUsername = FindViewById<EditText>(Resource.Id.etUsername);
+            var etPassword = FindViewById<EditText>(Resource.Id.etPassword);
 
-			button.Click += delegate { HTTPHelper.GetInstance().GetRequest("getMostActiveUsers", null, success: (obj) =>
-			{
-				//TODO: Success
-			}, failure: (json) =>
-			{
-				//TODO: Failure	
-			});
-			};
-		}
+            btnLogin.Click += delegate
+            {
+                Toast.MakeText(this, string.Format("Usuario: {0} Contraseña: {1}", etUsername.Text, etPassword.Text), ToastLength.Short).Show();
+            };
+
+            btnForgotPassword.Click += delegate
+            {
+                Toast.MakeText(this, "¡Qué lástima!", ToastLength.Short).Show();
+            };
+
+            //         btnLogin.Click += delegate { HTTPHelper.GetInstance().GetRequest("getMostActiveUsers", null, success: (obj) =>
+            //{
+            //	//TODO: Success
+            //}, failure: (json) =>
+            //{
+            //	//TODO: Failure	
+            //});
+            //};
+        }
 	}
 }
 
