f57ea2f5648be7098981592ef94512b04de79b5e
Alejandro Monetti <alejandromonetti13@gmail.com>
2016-07-25 23:02:58 -0300

    Integracion del login

--- DistriBot/Activities/MainActivity.cs
+++ DistriBot/Activities/MainActivity.cs
@@ -2,12 +2,15 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Content;
 
 namespace DistriBot
 {
 	[Activity(Label = "DistriBot", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar.FullScreen")]
 	public class MainActivity : AppCompatActivity
 	{
+
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
@@ -15,8 +18,6 @@ namespace DistriBot
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
 			var btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
             var btnForgotPassword = FindViewById<TextView>(Resource.Id.tvForgotPasswordLink);
             var etUsername = FindViewById<EditText>(Resource.Id.etUsername);
@@ -24,7 +25,21 @@ namespace DistriBot
 
             btnLogin.Click += delegate
             {
-                Toast.MakeText(this, string.Format("Usuario: {0} Contraseña: {1}", etUsername.Text, etPassword.Text), ToastLength.Short).Show();
+				if (etUsername.Text == "" || etPassword.Text == "")
+				{
+					Toast.MakeText(this, "El nombre de usuario y/o la contraseña son incorrectos", ToastLength.Long).Show();
+				}
+				else 
+				{
+					SalesmanServiceManager.Login(etUsername.Text, etPassword.Text, success: () => 
+					{
+						var menuActivity = new Intent(this, typeof(MenuActivity));
+						StartActivity(menuActivity);
+					}, failure: () => 
+					{
+						Toast.MakeText(this, "El nombre de usuario y/o la contraseña son incorrectos", ToastLength.Long).Show();	
+					});
+				}
             };
 
             btnForgotPassword.Click += delegate
@@ -32,14 +47,6 @@ namespace DistriBot
                 Toast.MakeText(this, "¡Qué lástima!", ToastLength.Short).Show();
             };
 
-            //         btnLogin.Click += delegate { HTTPHelper.GetInstance().GetRequest("getMostActiveUsers", null, success: (obj) =>
-            //{
-            //	//TODO: Success
-            //}, failure: (json) =>
-            //{
-            //	//TODO: Failure	
-            //});
-            //};
         }
 	}
 }
