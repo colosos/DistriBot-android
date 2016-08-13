ced80d502144fb8fefd6c6a835f2faaef11948f5
federicozaiter <fzaiter@gmail.com>
2016-08-13 17:20:53 -0300

    RecyclerView de productos con itemclick, item highlight al presionarlo y onscroll para endless scrolling

--- DistriBot/Activities/ProductsActivity.cs
+++ DistriBot/Activities/ProductsActivity.cs
@@ -10,20 +10,33 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Support.V7.App;
+using Android.Support.V7.Widget;
 
 namespace DistriBot
 {
 	[Activity(Label = "Productos", MainLauncher = true, Theme = "@style/DefaultTheme")]
 	public class ProductsActivity : AppCompatActivity
 	{
-		ListView listView;
-		List<Product> products = new List<Product>()
+        private RecyclerView mRecyclerView;
+        private LinearLayoutManager mLayoutManager;
+        private ProductsRecyclerAdapter mAdapter;
+
+        List<Product> products = new List<Product>()
 		{
 			new Product(1, "Salame", 12.3),
 			new Product(2, "Queso", 34),
 			new Product(3, "Azucar", 53),
-			new Product(4, "Agua", 3.11)
-		};
+			new Product(4, "Agua", 3.11),
+            new Product(5, "Coca", 3.11),
+            new Product(6, "Pomelo", 3.11),
+            new Product(7, "Cerveza", 3.11),
+            new Product(8, "Hamburguesas", 3.11),
+            new Product(9, "Panchos", 3.11),
+            new Product(10, "Nuggets", 3.11),
+            new Product(11, "Papas", 3.11),
+            new Product(12, "Jamón", 3.11),
+            new Product(13, "Mortadela", 3.11)
+        };
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -31,19 +44,57 @@ namespace DistriBot
 
 			SetContentView(Resource.Layout.Products);
 
-			listView = FindViewById<ListView>(Resource.Id.List);
+           
+            
+            mAdapter = new ProductsRecyclerAdapter(products);
+            mAdapter.ItemClick += OnListItemClick;
 
-			listView.Adapter = new ProductsAdapter(this, products);
-			listView.ItemClick += OnListItemClick;
-		}
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            if (mRecyclerView != null)
+            {
+                mRecyclerView.HasFixedSize = true;
+                mLayoutManager = new LinearLayoutManager(this);
 
-		void OnListItemClick(object sender, AdapterView.ItemClickEventArgs e)
-		{
-			var p = products[e.Position];
-			Android.Widget.Toast.MakeText(this, p.Name, Android.Widget.ToastLength.Short).Show();
-		}
+                var onScrollListener = new RecyclerViewOnScrollListener(mLayoutManager);
+                onScrollListener.LoadMoreEvent += (object sender, EventArgs e) =>
+                {
+                    //Load more stuff here
+                    List<Product> addProducts = new List<Product>()
+                    {
+                        new Product(14, "Chocolate", 3.11),
+                        new Product(15, "Arroz", 3.11),
+                        new Product(16, "Banana", 3.11),
+                        new Product(17, "Manzana", 3.11),
+                        new Product(18, "Limón", 3.11)
+                    };
+                    products.AddRange(addProducts);
+                    mAdapter.NotifyItemRangeInserted(products.Count, addProducts.Count);
+                };
 
-		public override bool OnCreateOptionsMenu(IMenu menu)
+                mRecyclerView.AddOnScrollListener(onScrollListener);
+
+                mRecyclerView.SetLayoutManager(mLayoutManager);
+                mRecyclerView.SetAdapter(mAdapter);
+            }
+
+            
+        }
+
+        void OnListItemClick(object sender, int position)
+        {
+            if (position >= 0)
+            {
+                var p = products[position];
+                Android.Widget.Toast.MakeText(this, p.Name, Android.Widget.ToastLength.Short).Show();
+
+                // Elimino el item que se selecciona y se le avisa especificamente eso al recyclerview adapter
+                products.RemoveAt(position);
+                mAdapter.NotifyItemRemoved(position);
+
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
 		{
 			MenuInflater.Inflate(Resource.Menu.home, menu);
 			return base.OnCreateOptionsMenu(menu);
---
+++ DistriBot/Helpers/ProductsRecyclerAdapter.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+
+namespace DistriBot
+{
+    class ProductsRecyclerAdapter : RecyclerView.Adapter
+    {
+        List<Product> products;
+        public event EventHandler<int> ItemClick;
+
+        public ProductsRecyclerAdapter (List<Product> products) : base()
+		{
+            this.products = products;
+        }
+
+        public override int ItemCount
+        {
+            get
+            {
+                return products.Count;
+            }
+        }
+
+        void OnClick(int position)
+        {
+            if (ItemClick != null)
+                ItemClick(this, position);
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            var product = products[position];
+            ProductView myHolder = holder as ProductView;
+            myHolder.Name.Text = product.Name;
+            myHolder.UnitPrice.Text = product.UnitPrice.ToString();
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View row = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.ProductRow, parent, false);
+
+            TextView txtName = row.FindViewById<TextView>(Resource.Id.Text1);
+            TextView txtUnitPrice = row.FindViewById<TextView>(Resource.Id.Text2);
+
+            return new ProductView(row, OnClick) { Name = txtName, UnitPrice = txtUnitPrice};
+        }
+
+        public class ProductView : RecyclerView.ViewHolder
+        {
+            public View MainView { get; set; }
+            public TextView Name { get; set; }
+            public TextView UnitPrice { get; set; }
+            
+            public ProductView (View view, Action<int> listener) : base(view)
+            {
+                MainView = view;
+                view.Click += (sender, e) => listener(base.Position);
+            }
+        }
+
+    }
+}
