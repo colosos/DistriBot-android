9891e8e34d9d90861dd8b2a1485b91c75c0fa03d
Alejandro Monetti <alejandromonetti13@gmail.com>
2016-08-25 19:03:42 -0300

    Adaptacion del login para el repartidor

--- DistriBot/Activities/MainActivity.cs
+++ DistriBot/Activities/MainActivity.cs
@@ -3,50 +3,61 @@ using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Content;
+using System.Threading;
 
 namespace DistriBot
 {
-	[Activity(Label = "DistriBot", Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar.FullScreen")]
+	[Activity(Label = "DistriBot", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar.FullScreen")]
 	public class MainActivity : AppCompatActivity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
 			var btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
             var btnForgotPassword = FindViewById<TextView>(Resource.Id.tvForgotPasswordLink);
-            var etUsername = FindViewById<EditText>(Resource.Id.etUsername);
-            var etPassword = FindViewById<EditText>(Resource.Id.etPassword);
 
-            btnLogin.Click += delegate
-            {
-				if (etUsername.Text == "" || etPassword.Text == "")
-				{
-					Toast.MakeText(this, "El nombre de usuario y/o la contraseña son incorrectos", ToastLength.Long).Show();
-				}
-				else 
-				{
-					SalesmanServiceManager.Login(etUsername.Text, etPassword.Text, success: () => 
-					{
-						var menuActivity = new Intent(this, typeof(MenuActivity));
-						StartActivity(menuActivity);
-					}, failure: () => 
-					{
-						Toast.MakeText(this, "El nombre de usuario y/o la contraseña son incorrectos", ToastLength.Long).Show();	
-					});
-				}
-            };
-
+			btnLogin.Click += BtnLogin_Click;
+            
             btnForgotPassword.Click += delegate
             {
                 Toast.MakeText(this, "¡Qué lástima!", ToastLength.Short).Show();
             };
 
         }
+
+		void BtnLogin_Click(object sender, System.EventArgs e)
+		{
+			var etUsername = FindViewById<EditText>(Resource.Id.etUsername);
+			var etPassword = FindViewById<EditText>(Resource.Id.etPassword);
+
+			if (etUsername.Text == "" || etPassword.Text == "")
+			{
+				Toast.MakeText(this, "El nombre de usuario y/o la contraseña son incorrectos", ToastLength.Long).Show();
+			}
+			else
+			{
+				
+
+
+				LoginServiceManager.Login(etUsername.Text, etPassword.Text, success: (role) =>
+				{
+					if (role.Equals("Salesman"))
+					{
+						var menuActivity = new Intent(this, typeof(MenuActivity));
+						StartActivity(menuActivity);
+					}
+					else
+					{
+						// TODO: Redirect to Deliveryman menu
+					}
+				}, failure: () =>
+				{
+					Toast.MakeText(this, "El nombre de usuario y/o la contraseña son incorrectos", ToastLength.Long).Show();
+				});
+
+			}
+		}
 	}
 }
-
-
